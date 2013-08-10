576d2ecbffafadc2235b8caf248e2187411c3fb9
MrDavidot <david.t@glazenkamp.net>
2013-08-10 15:15:35 +0200

    first commmit by me

--- LeGame/src/Start.java
+++ LeGame/src/Start.java
@@ -4,6 +4,11 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String bram = "bram is jarvar master";
+		if(bram.contains("master")){
+			System.out.println("Wow that is cool");
+		}
+		
 	}
 
 }
