456f816826db8bf38bf0d94ce51dbe1b24d7d7fd
MrDavidot <david.t@glazenkamp.net>
2013-08-10 15:41:50 +0200

    fixed render

--- LeGame/src/main/Game.java
+++ LeGame/src/main/Game.java
@@ -1,6 +1,8 @@
 package main;
 
+import java.awt.BorderLayout;
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -14,8 +16,8 @@ public class Game extends Canvas{
 	private static final long serialVersionUID = 1L;
 
 	public static String TITLE = "LeGame";
-	public static final int WIDTH = 250;
-	public static final int LEGTH = 250;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 500;
 	
 	
 	
@@ -27,8 +29,9 @@ public class Game extends Canvas{
 	
 	
 	public void render(){
-		BufferStrategy buffer = getBufferStrategy();
+		BufferStrategy buffer = this.getBufferStrategy();
 		if(buffer==null){
+			System.out.println("Eerst");
 			this.createBufferStrategy(2);
 			requestFocus();
 			return;
@@ -36,6 +39,7 @@ public class Game extends Canvas{
 		BufferedImage img = new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_ARGB);
 		Graphics2D g = img.createGraphics();
 		//Put stuff here to render
+		g.setColor(Color.RED);
 		g.fillOval(15, 15, 15, 15);
 		
 		
@@ -47,9 +51,7 @@ public class Game extends Canvas{
 		gBase.clearRect(0, 0, ww, hh);
 		gBase.drawImage(img, 0, 0, null);
 		gBase.dispose();
-		if(!buffer.contentsLost()){
-			buffer.show();
-		}
+		buffer.show();
 	}
 	
 	
@@ -58,16 +60,27 @@ public class Game extends Canvas{
 	
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		Game game = new Game();
-		game.setSize(new Dimension(Game.WIDTH,Game.HEIGHT));
-		game.setPreferredSize(new Dimension(Game.WIDTH,Game.HEIGHT));
+		game.setMinimumSize(new Dimension(WIDTH,HEIGHT));
+		game.setMaximumSize(new Dimension(WIDTH,HEIGHT));
+		game.setPreferredSize(new Dimension(WIDTH,HEIGHT));
 		
 		JFrame frame = new JFrame(Game.TITLE);
-		frame.add(game);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setMinimumSize(new Dimension(WIDTH,HEIGHT));
+		frame.setLayout(new BorderLayout());
+		frame.add(game,BorderLayout.CENTER);
+		frame.setResizable(false);
 		frame.pack();
+		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
+		game.render();
+		game.render();
+		game.render();
+		game.render();
+		game.render();
+		
 		
 	}
 
