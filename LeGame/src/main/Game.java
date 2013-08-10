26fc4a13ee196ec2a392af3e0718fd6eebf34a37
MrDavidot <david.t@glazenkamp.net>
2013-08-10 15:31:06 +0200

    first draw

--- LeGame/src/main/Game.java
+++ LeGame/src/main/Game.java
@@ -1,6 +1,11 @@
 package main;
 
 import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
 
@@ -9,12 +14,54 @@ public class Game extends Canvas{
 	private static final long serialVersionUID = 1L;
 
 	public static String TITLE = "LeGame";
+	public static final int WIDTH = 250;
+	public static final int LEGTH = 250;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public void render(){
+		BufferStrategy buffer = getBufferStrategy();
+		if(buffer==null){
+			this.createBufferStrategy(2);
+			requestFocus();
+			return;
+		}
+		BufferedImage img = new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g = img.createGraphics();
+		//Put stuff here to render
+		g.fillOval(15, 15, 15, 15);
+		
+		
+		//stop with stuff to render
+		int ww = getWidth();
+		int hh = getHeight();
+		Graphics gBase = buffer.getDrawGraphics();
+		gBase.fillRect(0, 0, ww, hh);
+		gBase.clearRect(0, 0, ww, hh);
+		gBase.drawImage(img, 0, 0, null);
+		gBase.dispose();
+		if(!buffer.contentsLost()){
+			buffer.show();
+		}
+	}
+	
+	
+	
 	
 	
 	
 	
 	public static void main(String[] args) {
 		Game game = new Game();
+		game.setSize(new Dimension(Game.WIDTH,Game.HEIGHT));
+		game.setPreferredSize(new Dimension(Game.WIDTH,Game.HEIGHT));
 		
 		JFrame frame = new JFrame(Game.TITLE);
 		frame.add(game);
