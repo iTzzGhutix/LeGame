cc3a6449b634568001e39f76563520f2d45aea8c
davidot <david.t@glazenkamp.net>
2013-08-10 22:33:40 +0200

    commit check

--- LeGame/src/david/DavidCrazyStuff.java
+++ LeGame/src/david/DavidCrazyStuff.java
@@ -1,5 +1,6 @@
 package david;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,19 +10,24 @@ import javax.swing.JFrame;
 public class DavidCrazyStuff {
 	//USA & UK CONVERTER
 	
+	Method m;
+	
 	private JFrame frame = new JFrame("Converter");
 	private List<Kleinheid> keys = new ArrayList<Kleinheid>();
 	
 	
 	public DavidCrazyStuff(){
-		
 	}
 	
 	public void init(){
 		
+		
+		
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
 	}
-	
-	
 	
 	
 	public static void main(String[] args){
--- LeGame/src/david/Kleinheid.java
+++ LeGame/src/david/Kleinheid.java
@@ -1,5 +1,28 @@
 package david;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Kleinheid {
 
+	public String name;
+	public int rek;
+	public List<Kleinheid> compat = new ArrayList<Kleinheid>();
+	
+	public Kleinheid(String name, int rek){
+		this.name = name;
+		this.rek = rek;
+	}
+	
+	public void addCompat(Kleinheid[] com){
+		for(Kleinheid k:com){
+			compat.add(k);
+		}
+	}
+	
+	
+	public String toString(){
+		return name;
+	}
+	
 }
