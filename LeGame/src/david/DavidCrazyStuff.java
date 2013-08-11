d7eec3e10317aea70914ceb508947c75bc3c6193
davidot <david.t@glazenkamp.net>
2013-08-11 13:13:40 +0200

    update for today

--- LeGame/src/david/DavidCrazyStuff.java
+++ LeGame/src/david/DavidCrazyStuff.java
@@ -1,11 +1,13 @@
 package david;
 
+import java.awt.FlowLayout;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JList;
 import javax.swing.JTextField;
 
 
@@ -14,21 +16,55 @@ public class DavidCrazyStuff {
 	
 	Method m;
 	
+	private Kleinheid[] kleinheid = Kleinheid.getAll();
+	
 	private JFrame frame = new JFrame("Converter");
-	private List<Kleinheid> keys = new ArrayList<Kleinheid>();
+	private List<Kleinheid> kleinheden = new ArrayList<Kleinheid>();
 	
 	
-	private JTextField input = new JTextField();
-	private JButton calc = new JButton();
+	private JTextField input = new JTextField("Input",8);
+	private JButton calc = new JButton("Calculate");
+	private JList<String> engList = new JList<String>();
+	private List<Kleinheid> engListK = new ArrayList<Kleinheid>();
+	private JList<String> mtrList = new JList<String>();
+	private List<Kleinheid> mtrListK = new ArrayList<Kleinheid>();
+	private JTextField output = new JTextField(8);
+	private List<Kleinheid> currentMtr = new ArrayList<Kleinheid>();
 	
 	public DavidCrazyStuff(){
 	}
 	
 	public void init(){
+		input.setSize(20, 80);
+		output.setSize(20, 80);
+		engList.setSize(20, 80);
+		mtrList.setSize(20, 80);
+		calc.setSize(20, 50);
+		for(Kleinheid k : kleinheid){
+			kleinheden.add(k);
+		}
+		for(Kleinheid k : kleinheden){
+			k.addCompat(kleinheden);
+			if(k.metric){
+				mtrListK.add(k);
+			}else{
+				engListK.add(k);
+			}
+		}
+		String[] eng = new String[engListK.size()];
+		for (int i = 0; i < engListK.size(); i++) {
+			eng[i] = engListK.get(i).name;
+		}			
+		
+		engList.setListData(eng);
 		
 		
-		
-		
+		frame.setLayout(new FlowLayout());
+		frame.add(engList);
+		frame.add(input);
+		frame.add(mtrList);
+		frame.add(calc);
+		frame.add(output);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
 		frame.setVisible(true);
--- LeGame/src/david/Kleinheid.java
+++ LeGame/src/david/Kleinheid.java
@@ -8,15 +8,21 @@ public class Kleinheid {
 	public String name;
 	public double rek;
 	public List<Kleinheid> compat = new ArrayList<Kleinheid>();
+	public boolean metric;
+	public Grootheid grootheid;
 	
-	public Kleinheid(String name, double rek){
+	public Kleinheid(String name, double rek,Grootheid g, boolean metric){
 		this.name = name;
 		this.rek = rek;
+		this.metric = metric;
+		grootheid = g;
 	}
 	
-	public void addCompat(Kleinheid[] com){
+	public void addCompat(List<Kleinheid> com){
 		for(Kleinheid k:com){
-			compat.add(k);
+			if(k.grootheid==grootheid&&!k.name.equalsIgnoreCase(name)){
+				compat.add(k);
+			}
 		}
 	}
 	
@@ -27,9 +33,26 @@ public class Kleinheid {
 		return -1;
 	}
 	
+	public List<Kleinheid> getCompat(){
+		return compat;
+	}
 	
 	public String toString(){
 		return name;
+	}
+
+	public static Kleinheid[] getAll() {
+		Kleinheid[] k = new Kleinheid[3];
+		k[0] = new Kleinheid("Centimeter" , 100,Grootheid.LENGTH,true );
+		k[1] = new Kleinheid("Meter" , 1,Grootheid.LENGTH,true );
+		k[2] = new Kleinheid("Kilometer" , 0.01,Grootheid.LENGTH,true );
+		
+		return k;
+	}
+	
+	public enum Grootheid {
+		LENGTH,
+		AREA
 	}
 	
 }
